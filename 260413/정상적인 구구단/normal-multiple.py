f8357cebe7716dc1815cac7f7f8d2d91238a26f0
kauhja <moongtang07@gmail.com>
2026-04-13 19:29:32 +0900

    Title: 정상적인 구구단, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260413/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(f"{i} x {j} = {i*j}", end='')
+        if j != n+1:
+            print(', ', end='')
+        else:
+            print()
